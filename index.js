ca533ff0c5a2bbf2e09438726c26d1fdb4471ed7
Yash Rajput <yash.pal_cs20@gla.ac.in>
2022-07-11 20:44:12 +0530

    mycoding commit

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("name").innerHTML="Yash Pal Singh";
+document.getElementById("rollno").innerHTML="201500822";
+const d=new Date();
+document.getElementById("date").innerHTML=d.toDateString();
